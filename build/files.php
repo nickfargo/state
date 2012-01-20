aecca9b300d803b895b336f62c21563e097005a1
Nick Fargo <nick@zvector.com>
2012-01-20 09:32:51 -0800

    remove old deferral includes

--- build/files.php
+++ build/files.php
@@ -1,11 +1,6 @@
 <?php
 $files = array(
 	'_util',
-	'../../deferral.js/src/deferral',
-	'../../deferral.js/src/promise',
-	'../../deferral.js/src/pipeline',
-	'../../deferral.js/src/multiplex',
-	'../../deferral.js/src/procedure',
 	'state',
 	'state-definition',
 	'state-controller',
