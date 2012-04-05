1d2ffa6b9e604b8b4b026bfd6cc6c3e4ba6a5796
Nick Fargo <nick.fargo@gmail.com>
2012-04-05 05:10:37 -0700

    add `conclusive` attribute, rename `regioned` to `concurrent`

--- state.js
+++ state.js
@@ -67,63 +67,70 @@ var STATE_ATTRIBUTES = {
         // should assume.
         INITIAL     : 0x2,
 
+        // Once a state marked `conclusive` is entered, it cannot be exited, although transitions
+        // may still freely traverse within its substates.
+        CONCLUSIVE  : 0x4,
+
         // Once a state marked `final` is entered, no further outbound transitions within its local
         // region are allowed.
-        FINAL       : 0x4,
+        FINAL       : 0x8,
 
         // An **abstract state** cannot itself be current. Consequently a transition target that
         // points to a state marked `abstract` is redirected to one of its substates.
-        ABSTRACT    : 0x8,
+        ABSTRACT    : 0x10,
 
         // Marking a state `default` designates it as the actual target for any transition that
         // targets its abstract superstate.
-        DEFAULT     : 0x10,
+        DEFAULT     : 0x20,
 
         // A state marked `sealed` cannot have substates.
-        SEALED      : 0x20,
+        SEALED      : 0x40,
 
         // A `retained` state is one that preserves its own internal state, such that, after the
         // state has become no longer active, a subsequent transition targeting that particular
         // state will automatically be redirected to whichever of its descendant states was most
         // recently current.
         // *(Reserved; not presently implemented.)*
-        RETAINED    : 0x40,
+        RETAINED    : 0x80,
 
         // Marking a state with the `history` attribute causes its internal state to be recorded
         // in a sequential **history**. Whereas a `retained` state is concerned only with the most
         // recent internal state, a state’s history can be traversed and altered, resulting in
         // transitions back or forward to previously or subsequently held internal states.
         // *(Reserved; not presently implemented.)*
-        HISTORY     : 0x80,
+        HISTORY     : 0x100,
 
         // Normally, states that are `retained` or that keep a `history` persist their internal
         // state *deeply*, i.e., with a scope extending over all of the state’s descendant states.
         // Marking a state `shallow` limits the scope of its persistence to its immediate
         // substates only.
         // *(Reserved; not presently implemented.)*
-        SHALLOW     : 0x100,
+        SHALLOW     : 0x200,
 
         // Causes alterations to a state to result in a reflexive transition, with a delta object
         // distinguishing the prior version of the state from its new version. Should also add a
         // history entry wherever appropriate, representing the prior version and the delta.
         // *(Reserved; not presently implemented.)*
-        VERSIONED   : 0x200,
+        VERSIONED   : 0x400,
 
-        // In a state marked `regioned`, the substates are considered **concurrent orthogonal
-        // regions**. Upon entering a regioned state, the controller creates a new set of
-        // subcontrollers, one for each region, which will exist as long as the regioned state
+        // In a state marked `concurrent`, the substates are considered **concurrent orthogonal
+        // regions**. Upon entering a concurrent state, the controller creates a new set of
+        // subcontrollers, one for each region, which will exist as long as the concurrent state
         // remains active. Method calls are forwarded to at most one of the regions, or if a
         // reduction function is associated with the given method, the call is repeated for each
-        // region and the results reduced accordingly on their way back to the owner. Event
-        // emissions, which have void return types, are simply propagated to all of the regions.
+        // region and the results reduced accordingly on their way back to the owner.
         // *(Reserved; not presently implemented.)*
-        REGIONED    : 0x400
+        CONCURRENT  : 0x800
     },
 
     // The subset of attributes that are valid keywords for the `attributes` argument in a call to
     // the exported `state` function.
-    STATE_ATTRIBUTE_MODIFIERS =
-        'initial final abstract default sealed retained history shallow versioned regioned',
+    STATE_ATTRIBUTE_MODIFIERS = [
+        'initial conclusive final',
+        'abstract default sealed',
+        'retained history shallow versioned',
+        'concurrent'
+    ].join(' '),
     
     // 
     STATE_EXPRESSION_CATEGORIES =
@@ -1355,7 +1362,8 @@ var State = ( function () {
                     if ( state.match( Z.trim( expr ), testState ) ) {
                         result = !!( typeof value === 'function' ?
                             value.call( state, testState ) :
-                            value );
+                            value
+                        );
                         return false; 
                     }
                 });
