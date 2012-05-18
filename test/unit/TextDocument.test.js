2dbfc21bcd30be11663c4e14454a3911336058c7
Nick Fargo <nick.fargo@gmail.com>
2012-05-18 11:43:35 -0700

    formatting / cleanup

--- test/unit/TextDocument.test.js
+++ test/unit/TextDocument.test.js
@@ -7,55 +7,55 @@
 		return this.owner();
 	};
 
-	var TextDocument = (function() {
+	var TextDocument = ( function() {
 
-		function TextDocument(url) {
+		function TextDocument ( url ) {
 			var text;
 			this.url = url;
 			text = '';
-			this.text = function() {
+			this.text = function () {
 				return text;
 			};
-			this.edit = function(newText) {
+			this.edit = function ( newText ) {
 				text = newText;
 				return this;
 			};
 			this.state();
 		}
 
-		TextDocument.prototype.save = function() {
-			io.write(this.url, this.text());
+		TextDocument.prototype.save = function () {
+			io.write( this.url, this.text() );
 			return this;
 		};
 
-		state(TextDocument.prototype, {
-			test: function() {
-				return console.log(this.name());
+		state( TextDocument.prototype, {
+			test: function () {
+				return console.log( this.name() );
 			},
-			freeze: function() {
+			freeze: function () {
 				var result;
 				result = this.owner().save();
 				this.change('Saved.Frozen');
 				return result;
 			},
-			Saved: state('initial', {
-				edit: function(newText) {
+			Saved: state( 'initial', {
+				edit: function ( newText ) {
 					var result;
-					result = this.superstate().call('edit', newText);
+					result = this.superstate().call( 'edit', newText );
 					this.change('Dirty');
 					return result;
 				},
 				save: owner,
 				enter: function () {},
 				exit: function () {},
-				Frozen: state('final sealed', {
+				Frozen: state( 'final sealed', {
 					edit: owner,
 					freeze: owner,
 					arrive: function () {}
 				})
 			}),
 			Dirty: {
-				save: function() {
+				save: function () {
 					var result;
 					result = this.superstate().call('save');
 					this.change('Saved');
@@ -80,12 +80,24 @@ test( "TextDocument", function () {
 	var	doc1 = new TextDocument,
 		doc2 = new TextDocument;
 
-	doc1.state().go('Saved'); doc2.state().go('Saved');
-	assert.strictEqual( doc1.state(), doc1.state('Saved'), "Initial state active" );
+	doc1.state().go('Saved');
+	doc2.state().go('Saved');
+	assert.ok(
+		doc1.state().is('Saved'),
+		"Initial state active"
+	);
+	
 	doc1.edit('foo');
-	assert.strictEqual( doc1.state(), doc1.state('Dirty'), "Edit causes transition from 'Saved' to 'Dirty'" );
+	assert.ok(
+		doc1.state().is('Dirty'),
+		"Edit causes transition from 'Saved' to 'Dirty'"
+	);
+	
 	doc1.freeze();
-	assert.strictEqual( doc1.state(), doc1.state('Saved.Frozen'), "`freeze` callable from 'Dirty', causes transition to 'Saved.Frozen'" );
+	assert.ok(
+		doc1.state().is('Frozen'),
+		"`freeze` callable from 'Dirty', causes transition to 'Saved.Frozen'"
+	);
 });
 
 })( QUnit );
